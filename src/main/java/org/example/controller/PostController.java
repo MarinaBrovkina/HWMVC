7d3c0f0707e67f1350bfc860ed16a2b64c20ef28
MarinaBrovkina <mbrovk@gmail.com>
2024-06-04 15:56:10 +0300

    Second commit

--- src/main/java/org/example/controller/PostController.java
+++ src/main/java/org/example/controller/PostController.java
@@ -1,7 +1,6 @@
 package org.example.controller;
 
 import com.google.gson.Gson;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.example.model.Post;
 import org.example.service.PostService;
@@ -10,11 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Controller
+@RestController
 @RequestMapping("/api/posts")
 public class PostController {
     public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
+    final Gson gson = new Gson();
 
     public PostController(PostService service) {
         this.service = service;
@@ -24,19 +24,19 @@ public class PostController {
     public void all(HttpServletResponse response) throws IOException {
         response.setContentType(APPLICATION_JSON);
         final var data = service.all();
-        final var gson = new Gson();
         response.getWriter().print(gson.toJson(data));
     }
 
     @GetMapping("/{id}")
-    public void getById(@PathVariable long id, HttpServletResponse response) {
-        // TODO: deserialize request & serialize response
+    public void getById(@PathVariable long id, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var post = service.getById(id);
+        response.getWriter().print(gson.toJson(post));
     }
 
     @PostMapping
     public void save(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType(APPLICATION_JSON);
-        final var gson = new Gson();
         final var post = gson.fromJson(request.getReader(), Post.class);
         final var data = service.save(post);
         response.getWriter().print(gson.toJson(data));
@@ -44,6 +44,7 @@ public class PostController {
 
     @DeleteMapping("/{id}")
     public void removeById(@PathVariable long id, HttpServletResponse response) {
-        // TODO: deserialize request & serialize response
+        service.removeById(id);
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
     }
 }
